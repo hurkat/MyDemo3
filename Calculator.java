fec23f763bf930bd0ed65199ae6a624aab6b65de
Manish Hurkat <manishhurkat@yahoo.com>
2021-10-14 14:44:13 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 0, result = 0;
+		System.out.print("Enter number 1 : ");
+		int a = sc.nextInt();
+		System.out.print("Enter number 2 : ");
+		int b = sc.nextInt();
+		System.out.println("1. Add");
+		System.out.println("2. Sub");
+		System.out.println("3. Mult");
+		System.out.println("4. Div");
+		System.out.print("Enter choice : ");
+		int ch = sc.nextInt();
+		switch (ch) {
+		// case 1:
+		}
+
+	}
+}
